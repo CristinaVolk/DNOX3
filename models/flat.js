91b570df7ba0a305d98a87b0a7ad252acad72156
Cristina <kristinavolk4@tut.by>
2018-04-25 20:12:34 +0200

    маленькое изменение

--- models/flat.js
+++ models/flat.js
@@ -1,7 +1,16 @@
 const mongoose = require('mongoose');
-//var bcrypt = require('bcryptjs');
 let Schema = mongoose.Schema;
-
+const RoomSchema = new Schema({
+  title:String,
+  number:Number,
+  description:String,
+  price:Number,
+  number_of_rooms:Number,
+  number_of_free_rooms:Number,
+  data:{
+    type:Date
+  }
+});
 const FlatSchema = new Schema({
   location:{
     type: String,
@@ -19,11 +28,15 @@ const FlatSchema = new Schema({
     type: String,
     required: true,
   },
-  Rooms:[String]
+  Rooms:[RoomSchema],
+  data:{
+    type:Date,
+    default:Date.now()
+  }
 });
-
 
 const connectWithDB = (connection) => {
   const Flat = connection.model('Flat', FlatSchema, 'Flat');
+
 }
  module.exports = connectWithDB;
